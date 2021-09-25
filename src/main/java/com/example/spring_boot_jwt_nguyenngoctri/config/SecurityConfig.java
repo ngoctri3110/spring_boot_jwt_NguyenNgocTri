283778f678427f48ddaead92b0336b867adac4ff
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2021-09-25 17:23:19 +0700

    add filter-testPermission

--- src/main/java/com/example/spring_boot_jwt_nguyenngoctri/config/SecurityConfig.java
+++ src/main/java/com/example/spring_boot_jwt_nguyenngoctri/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.example.spring_boot_jwt_nguyenngoctri.config;
 
+import com.example.spring_boot_jwt_nguyenngoctri.filter.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf()
@@ -14,5 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/register")
                 .permitAll();
+
+        http.addFilterBefore(jwtRequestFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+                .csrf().disable();
     }
 }
--- src/main/java/com/example/spring_boot_jwt_nguyenngoctri/controller/AuthController.java
+++ src/main/java/com/example/spring_boot_jwt_nguyenngoctri/controller/AuthController.java
@@ -9,10 +9,9 @@ import com.example.spring_boot_jwt_nguyenngoctri.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AuthController {
@@ -54,6 +53,29 @@ public class AuthController {
         tokenService.createToken(token);
 
         return ResponseEntity.ok(token.getToken());
+    }
+
+    @GetMapping("/hello")
+    @PreAuthorize("hasAnyAuthority('USER_READ')")
+    public ResponseEntity hello(){
+        return ResponseEntity.ok("hello");
+    }
+
+    @PutMapping("/user")
+    @PreAuthorize("hasAnyAuthority('USER_UPDATE')")
+    public ResponseEntity update(){
+        return ResponseEntity.ok("This is update");
+    }
+    @PostMapping("/user")
+    @PreAuthorize("hasAnyAuthority('USER_CREATE')")
+    public ResponseEntity create(){
+        return ResponseEntity.ok("This is create");
+    }
+
+    @DeleteMapping("/user")
+    @PreAuthorize("hasAnyAuthority('USER_DELETE')")
+    public ResponseEntity delete(){
+        return ResponseEntity.ok("This is delete");
     }
 
 }
